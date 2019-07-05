c7651741d023f58a6a9024433ae162a80f9e5978
Neil Mountford <2104393+OmNomRarg@users.noreply.github.com>
2019-07-05 16:11:54 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Pi!");
+
+            // If you put a breakpoint here it should work remotely! :D
+            Console.WriteLine("That was some delicious Pi!");
+
+            Console.WriteLine("But now the pi is all nommed, WAIT NO ITS NOT!!!");
+        }
+    }
+}
---
+++ publish.bat
@@ -0,0 +1,2 @@
+dotnet publish
+scp -r ./bin/Debug/netcoreapp2.2/publish/* pi@raspberrypi.local:/home/pi/development/HelloPi
